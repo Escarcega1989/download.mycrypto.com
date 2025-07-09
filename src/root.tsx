84a17cb9c2a0a7c9d145144ceae5703cd65bd948
Escarcega1989 <escarcegalisamarie7@gmail.com>
2025-07-09 04:22:59 -0700

    root.change

--- src/root.tsx
+++ src/root.tsx
@@ -4,5 +4,6 @@ import App from 'app';
 import 'what-input';
 import 'sass/styles.scss';
 
-const root = document.getElementById('root');
+const root = document.getElementById('root')
+
 ReactDOM.render(<App />, root);
